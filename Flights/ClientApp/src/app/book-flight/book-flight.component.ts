c99a85437c5b169dd68e2a9cc42372f1e81277b2
LewisTodesco <lewis.todesco@googlemail.com>
2023-05-31 21:57:31 +0100

    Add project files.

---
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlightService } from './../api/services/flight.service';
+import { FlightRm } from './../api/models';
+
+@Component({
+  selector: 'app-book-flight',
+  templateUrl: './book-flight.component.html',
+  styleUrls: ['./book-flight.component.css']
+})
+export class BookFlightComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private flightService: FlightService) { }
+
+  flightId: string = 'not loaded'
+  flight: FlightRm = {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(p => this.findFlight(p.get("passedFlightId")))
+  }
+
+  private findFlight = (flightId: string | null) => {
+    this.flightId = flightId ?? 'not passed';
+
+    this.flightService.findFlight({ id: this.flightId }).subscribe(foundFlight => this.flight = foundFlight, this.handleError)
+  }
+
+  private handleError = (err: any) => {
+
+    if (err.status == 404)
+    {
+      alert("Flight not found!")
+      this.router.navigate(['/search-flights'])
+    }
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err)
+  }
+
+
+}
---
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FlightService } from './../api/services/flight.service'
+import { FlightRm } from './../api/models';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent {
+
+  searchResult: FlightRm[] = []
+
+  constructor(private flightService: FlightService) { }
+
+  search() {
+    this.flightService.searchFlight({})
+      .subscribe(response => this.searchResult = response, this.handleError)
+  }
+
+  private handleError(err: any) {
+
+    if (err.status == 404) {
+      alert("Flight not found!")
+    }
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err)
+  }
+
+}
